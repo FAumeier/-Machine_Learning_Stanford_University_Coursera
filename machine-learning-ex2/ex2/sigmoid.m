16e6dd065e82e2706a66c50fa3c23614f25b22d0
FAumeier <aumeier@protonmail.com>
2016-09-25 08:42:34 +0200

    sigmoid.m kann jetzt mit matrizen und vektoren umgehen.

--- machine-learning-ex2/ex2/sigmoid.m
+++ machine-learning-ex2/ex2/sigmoid.m
@@ -8,8 +8,10 @@ g = zeros(size(z));
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the sigmoid of each value of z (z can be a matrix,
 %               vector or scalar).
-
-g = 1/(1+exp(z));
+% Get size of z
+%one_temp = size(z);
+  
+g = 1./(1+exp(z));
 
 % =============================================================
 
