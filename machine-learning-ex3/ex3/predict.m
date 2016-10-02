7dc91affc2dbabbd02a7d6bdfc3e2bb6d44484ea
Florian Aumeier <aumeier@protonmail.com>
2016-10-02 10:04:30 +0200

    Neural Network Prediction Function implemented 2.

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -7,12 +7,12 @@ function p = predict(Theta1, Theta2, X)
 m = size(X, 1);
 num_labels = size(Theta2, 1);
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 p = zeros(size(X, 1), 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
-%               your learned neural network. You should set p to a 
+%               your learned neural network. You should set p to a
 %               vector containing labels between 1 to num_labels.
 %
 % Hint: The max function might come in useful. In particular, the max
@@ -21,6 +21,16 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+%add column of 1's to x = a1
+a1 = [ones(m, 1) X];
+% Multiply by Theta1 and you have 'z2'.
+z2 = a1 * Theta1';
+% Compute the sigmoid() of 'z2' and add a column of 1's, and it becomes 'a2'
+a2 = [ones(size(z2, 1), 1) sigmoid(z2)];
+% Multiply by Theta2, compute the sigmoid() and it becomes 'a3'.
+a3 = sigmoid(a2 * Theta2');
+% Now use the max(a3, [], 2) function to return two vectors - one of the highest value for each row, and one with its index. Ignore the highest values. Keep the vector of the indexes where the highest values were found. These are your predictions.
+[v p] = max(a3, [], 2);
 
 
 
